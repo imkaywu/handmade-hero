0a739b11c479ac34a889916546a8d77bebbd1be1
imkaywu <imkaywu@gmail.com>
2025-11-13 17:43:34 +0800

    day002: opening a Win32 window

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@
 mkdir build
 pushd build
 
-g++ -std=c++20 -g -O0 ../code/win32_handmade.cpp -o main -luser32
+g++ -std=c++20 -g -O0 ../code/win32_handmade.cpp -o main -luser32 -lgdi32
 
 popd
--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,8 +1,89 @@
 #include <windows.h>
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                     LPSTR lpCmdLine, int nCmdShow) {
-  MessageBoxW(0, L"This is Handmade Hero.", L"Handmade Hero",
-             MB_OK | MB_ICONINFORMATION);
+LRESULT CALLBACK MainWindowCallback(HWND window,
+                                    UINT message,
+                                    WPARAM wparam,
+                                    LPARAM lparam) {
+  LRESULT result = 0;
+
+  switch (message) {
+    case WM_SIZE: {
+      OutputDebugStringA("WM_SIZE");
+    } break;
+    case WM_DESTROY: {
+      OutputDebugStringA("WM_DESTROY");
+    } break;
+    case WM_CLOSE: {
+      OutputDebugStringA("WM_CLOSE");
+    } break;
+    case WM_ACTIVATEAPP: {
+      OutputDebugStringA("WM_ACTIVATEAPP");
+    } break;
+    case WM_PAINT: {
+      PAINTSTRUCT paint;
+      HDC device_context = BeginPaint(window, &paint);
+      int x = paint.rcPaint.left;
+      int y = paint.rcPaint.top;
+      int width = paint.rcPaint.right - paint.rcPaint.left;
+      int height = paint.rcPaint.bottom - paint.rcPaint.top;
+      static DWORD operation = WHITENESS;
+      PatBlt(device_context, x, y, width, height, operation);
+      if (operation == WHITENESS) {
+        operation = BLACKNESS;
+      } else {
+        operation = WHITENESS;
+      }
+      EndPaint(window, &paint);
+    } break;
+    default: {
+      // OutputDebugStringA("default");
+      result = DefWindowProc(window, message, wparam, lparam);
+    } break;
+  }
+
+  return result;
+}
+
+int CALLBACK WinMain(HINSTANCE instance,
+                     HINSTANCE prev_instance,
+                     LPSTR cmd_line,
+                     int cmd_show) {
+  WNDCLASSA window_class = {};
+  window_class.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+  window_class.lpfnWndProc = MainWindowCallback;
+  window_class.hInstance = instance;
+  window_class.lpszClassName = "HandmadeHeroWindowClass";
+
+  if (RegisterClassA(&window_class)) {
+    HWND window_handle = CreateWindowExA(0,
+                                         window_class.lpszClassName,
+                                         "Handmade Hero",
+                                         WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                                         CW_USEDEFAULT,
+                                         CW_USEDEFAULT,
+                                         CW_USEDEFAULT,
+                                         CW_USEDEFAULT,
+                                         0,
+                                         0,
+                                         instance,
+                                         0);
+
+    if (window_handle) {
+      MSG message;
+      while (true) {
+        BOOL message_result = GetMessage(&message, 0, 0, 0);
+        if (message_result > 0) {
+          TranslateMessage(&message);
+          DispatchMessage(&message);
+        } else {
+          break;
+        }
+      }
+    } else {
+      // TODO: Logging
+    }
+  } else {
+    // TODO: Logging
+  }
   return 0;
 }
